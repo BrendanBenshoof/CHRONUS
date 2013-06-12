4b19735af2d68bc5adc17695af719e9cc117ab9c
= <=>
2013-06-12 14:41:06 -0400

    fixed tabification

--- CHRONUS/CODE/message.py
+++ CHRONUS/CODE/message.py
@@ -5,32 +5,37 @@ import pickle
 #you could use it, but it would be boring
 
 def deserialize(in_string):
-	return pickle.loads(in_string)
-	#there are soo many exceptions I should be catching here
+    return pickle.loads(in_string)
+    #there are soo many exceptions I should be catching here
 
 class Message(object):
-	def __init__(self):
-		self.contents = {}
-		self.service = None
-		self.messagetype = 0
-		self.destination_node = 0
-		self.origin_node = 0
+    def __init__(self):
+        self.contents = {}
+        self.service = None
+        self.messagetype = 0
+        self.destination_node = 0
+        self.origin_node = 0
 
-	def serialize(self):
-		#it would be great if this was encrypted
-		#would could also fix this with using a public-key algorithim for p2p communication
-		return pickle.dumps(self)
+    def serialize(self):
+        #it would be great if this was encrypted
+        #would could also fix this with using a public-key algorithim for p2p communication
+        return pickle.dumps(self)
 
-	def add_content(self, key, data):
-		self.contents[key] = data
+    def add_content(self, key, data):
+        self.contents[key] = data
 
-	def get_content(self, key):
-		to_return = None
-		try:
-			to_return = self.contents[key]
-		except IndexError:
-			print "get_content was asked to look for a non-existant key"
-		return to_return
+    def get_content(self, key):
+        to_return = None
+        try:
+            to_return = self.contents[key]
+        except IndexError:
+            print "get_content was asked to look for a non-existant key"
+        return to_return
 
 
+class Database_Message(Message):
+    def __init__(self, dest, origin_node, file_id):
+        super(Message, self)
+        self.destination_node = dest
+        self.service = "DATABASE"
 
--- CHRONUS/CODE/service.py
+++ CHRONUS/CODE/service.py
@@ -1,20 +1,20 @@
 from message import *
 from hash_util import *
 class Service(object):
-	"""This object is intented to act as a parent/promise for Service Objects"""
-	def __init__(self):
-		self.service_id = None
-		self.owner = None
-	def attach(self, owner):
-		"""Called when the service is attached to the node"""
-		"""Should return the ID that the node will see on messages to pass it"""
-		self.owner = owner
-		return self.service_id
+    """This object is intented to act as a parent/promise for Service Objects"""
+    def __init__(self):
+        self.service_id = None
+        self.owner = None
+    def attach(self, owner):
+        """Called when the service is attached to the node"""
+        """Should return the ID that the node will see on messages to pass it"""
+        self.owner = owner
+        return self.service_id
 
-	def handle_message(self, msg):
-		"""This function is called whenever the node recives a message bound for this service"""
-		"""Return True if message is handled correctly
-		Return False if things go horribly wrong
-		"""
-		return msg.service == self.service_id
-		
+    def handle_message(self, msg):
+        """This function is called whenever the node recives a message bound for this service"""
+        """Return True if message is handled correctly
+        Return False if things go horribly wrong
+        """
+        return msg.service == self.service_id
+        

458b3e1f1164f521498335469ccf5bbb3a22189c
Andrew Rosen <rosen@cs.gsu.edu>
2013-06-29 16:12:29 -0400

    shelver is almost done

--- CHRONUS/CODE/shelver.py
+++ CHRONUS/CODE/shelver.py
@@ -4,26 +4,33 @@ from threading import Lock
 import hash_util
 import shelve
 
+GET = "GET"
+PUT = "PUT"
+DATABASE = "DATABASE"
+ECHO = "ECHO"
+RESPONSE = "RESP"
+
 class Shelver(Service):
     """docstring for Database"""
     def __init__(self, db):
         super(Database, self).__init__()
-        self.service_id = "DATABASE"
+        self.service_id = DATABASE
         self.write_lock = Lock()
         self.db = db
 
     def lookup_record(self, hash_name):
         self.write_lock.acquire(blocking = False)
         records = shelve.open(self.db)
+        content None
         try:
             print "looking up:"+hash_name
             content = records[hash_name]    #this retrieved COPY OF CONTENT
-            return content
         except KeyError: 
-            return "404 Error"
+            content =  "404 Error"
         finally:
             records.close()
             self.write_lock.release
+            return content
 
     def write_record(self, hash_name,content):
         self.write_lock.acquire()
@@ -31,20 +38,29 @@ class Shelver(Service):
         records[hash_name] = content
         records.close()
         self.write_lock.release()
+        return True
 
     def put_record(self,name,data):
         hash_loc = hash_util.hash_str(name)
-        newmsg = Database_Message(self.owner,hash_loc,"DATABASE","PUT")
+        newmsg = Database_Message(self.owner,hash_loc,DATABASE,PUT)
         newmsg.add_content("file_contents",data)
         self.callback(newmsg)
 
     def get_record(self,name):
         hash_loc = hash_util.hash_str(name)
-        newmsg = Database_Message(self.owner,hash_loc,"ECHO","GET")
+        newmsg = Database_Message(self.owner,hash_loc,ECHO,GET)
         self.callback(newmsg)
 
 
     def handle_message(self, msg):
         if not msg.service == self.service_id:
             return False
-
+        if msg.get_content("type")  == GET:
+            name = str(msg.destination_key)
+            content = self.lookup_record(name)  #str(index)
+            sendDataBack()
+        if msg.get_content("type")  == PUT:
+            name = str(msg.destination_key)
+            self.write_record(name,msg.get_content("file_contents"))
+        if msg.get_content("type") == RESPONSE:
+            print msg.get_content("file_contents")

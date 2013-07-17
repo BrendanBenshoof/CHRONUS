e7fffc3d28439ca3a9e0c799097484c49aca7d15
= <=>
2013-07-17 15:04:13 -0400

    actuall commiting now

--- CHRONUS/CODE/simple_network.py
+++ CHRONUS/CODE/simple_network.py
@@ -1,13 +1,62 @@
 #simpler networking solution
-import SocketServer
+from SocketServer import *
 import threading
 import socket
 import node
 import message
-import Queue
+from Queue import Queue
 import time
 
 CHUNKSIZE = 64
+
+##stolen from http://code.activestate.com/recipes/574454-thread-pool-mixin-class-for-use-with-socketservert/
+class ThreadPoolMixIn(ThreadingMixIn):
+    '''
+    use a thread pool instead of a new thread on every request
+    '''
+    numThreads = 10
+    allow_reuse_address = True  # seems to fix socket.error on server restart
+
+    def serve_forever(self):
+        '''
+        Handle one request at a time until doomsday.
+        '''
+        # set up the threadpool
+        self.requests = Queue(self.numThreads)
+
+        for x in range(self.numThreads):
+            t = threading.Thread(target = self.process_request_thread)
+            t.setDaemon(1)
+            t.start()
+
+        # server main loop
+        while True:
+            self.handle_request()
+            
+        self.server_close()
+
+    
+    def process_request_thread(self):
+        '''
+        obtain request from queue instead of directly from server socket
+        '''
+        while True:
+            ThreadingMixIn.process_request_thread(self, *self.requests.get())
+
+    
+    def handle_request(self):
+        '''
+        simply collect requests and put them on the queue for the workers.
+        '''
+        try:
+            request, client_address = self.get_request()
+        except socket.error:
+            return
+        if self.verify_request(request, client_address):
+            self.requests.put((request, client_address))
+
+class ThreadedServer(ThreadPoolMixIn, TCPServer):
+    pass
 
 
 class NETWORK_SERVICE(object):
@@ -24,8 +73,8 @@ class NETWORK_SERVICE(object):
 
     def __init__(self,HOST="localhost",PORT=9000):
         # Create the server, binding to localhost on port 9999
-        self.server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
-        self.tosend = Queue.Queue()
+        self.server = ThreadedServer((HOST, PORT), MyTCPHandler)
+        self.tosend = Queue()
         # Activate the server; this will keep running until you
         # interrupt the program with Ctrl-C
         t = threading.Thread(target=self.server.serve_forever)
@@ -98,7 +147,7 @@ class NETWORK_SERVICE(object):
             return True
 
 
-class MyTCPHandler(SocketServer.BaseRequestHandler):
+class MyTCPHandler(BaseRequestHandler):
     """
     The RequestHandler class for our server.
 
@@ -109,7 +158,7 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
 
     def __init__(self, request, client_address, server):
         self.data = ""
-        SocketServer.BaseRequestHandler.__init__(self, request, client_address, server)
+        BaseRequestHandler.__init__(self, request, client_address, server)
 
 
 

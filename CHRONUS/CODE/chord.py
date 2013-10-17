b4a9982bdd9f155ceed77c2d57708967357e609f
Brendan Benshoof <brendan@glidr.net>
2013-10-17 12:43:52 -0400

    turned up churn

--- CHRONUS/CODE/chord.py
+++ CHRONUS/CODE/chord.py
@@ -3,7 +3,7 @@
 from subprocess import Popen
 from time import sleep
 from random import random
-die_chance = 0.0000
+die_chance = 0.0008
 
 thingy = Popen(["./chordreduce.py", "?", "54.225.230.238","9000"])
 sleep(20)

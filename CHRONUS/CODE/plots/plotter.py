72ca0d5867464e2a4a4a84fab303c2e5bd584f07
BrendanBenshoof <brendan@glidr.net>
2013-10-18 16:37:16 -0400

    try these

--- CHRONUS/CODE/plots/plotter.py
+++ CHRONUS/CODE/plots/plotter.py
@@ -18,19 +18,18 @@ r0 = [44.81,49.5699999332,115.7799998919,235.4800000191,361.4900000095]
 r1 = [431.17,131.05,86.80,106.91,191.90]
 r2 = [4382.2,1082.1,483.21,430.78,686.43]
 
-<<<<<<< HEAD
 t0=r0[0]
 t1=r1[0]
 t2=r2[0]
 
-for i in range(0,len(r0)):
-    r0[i]=t0/r0[i]
+# for i in range(0,len(r0)):
+#     r0[i]=t0/r0[i]
 
-for i in range(0,len(r1)):
-    r1[i]=t1/r1[i]
+# for i in range(0,len(r1)):
+#     r1[i]=t1/r1[i]
 
-for i in range(0,len(r2)):
-    r2[i]=t2/r2[i]
+# for i in range(0,len(r2)):
+#     r2[i]=t2/r2[i]
 
 
 k0 = []
@@ -72,10 +71,10 @@ xnew = np.linspace(1.0,40.0,300)
 #plt.plot(n1,k1,".")
 #plt.plot(n2,k2,".")
 #plt.show()
-=======
+
 churn =     [0.008,  0.004,  0.000775, 0.00025, 0.000025, 0.0]
 churn_dur = [191.25, 329.20, 445.47,   431.8667,331.803,  411.57]
->>>>>>> a4d282ec83de5d94a1d6f91d9c9b819510b3e948
+
 
 
 xnew = np.linspace(min(churn),max(churn),300)
@@ -88,29 +87,21 @@ plt.xlabel('likelyhood of failure per second')
 plt.title("Duration versus churn")
 plt.show()
 
-<<<<<<< HEAD
-xnew = np.linspace(min(n2),max(n2),300)
-y2 = spline(n2,r2,xnew)
-#plt.figure(1)
-#a = plt.plot(xnew,y2,"-")
-plt.plot(n2,r2,"ok-", label="10^9 sample job")
-=======
->>>>>>> a4d282ec83de5d94a1d6f91d9c9b819510b3e948
 
 
-<<<<<<< HEAD
 xnew = np.linspace(min(n1),max(n1),300)
 y1 = spline(n1,r1,xnew)
 #plt.figure(2)
 #b = plt.plot(xnew,y1,"-")
-plt.plot(n1,r1,"+k-",label="10^8 sample job")
+plt.plot(n2,r2,"ok--",label="10^9 sample job")
+
+plt.plot(n1,r1,"ok-",label="10^8 sample job")
 xnew = np.linspace(min(n0),max(n0),300)
 y0 = spline(n0,r0,xnew)
 #plt.figure(3)
 #c = plt.plot(xnew,y0,"-")
-plt.plot(n0,r0,"xk-",label="10^7 sample job")
-=======
->>>>>>> a4d282ec83de5d94a1d6f91d9c9b819510b3e948
+plt.plot(n0,r0,"ok:",label="10^7 sample job")
+plt.plot([50.0],[0.0],"w")
 
 
 #t0=44.8099999427795
@@ -145,7 +136,6 @@ plt.plot(n0,r0,"xk-",label="10^7 sample job")
         ##r2.append(i)
         ##print n,i
 
-<<<<<<< HEAD
 # for i in range(3,10,1):
 #     t_0 = 10**i
 #     x = range(n_min,n_max)
@@ -155,16 +145,10 @@ plt.plot(n0,r0,"xk-",label="10^7 sample job")
 
 
 plt.legend()
-plt.ylabel('speedup')
+plt.ylabel('time (seconds)')
 plt.xlabel('number of workers')
-plt.title("Experimental speedup")
-=======
-##s = sum(k0)+sum(k1)+sum(k2)
-
-#xnew = np.linspace(1.0,40.0,300)
-
-
->>>>>>> a4d282ec83de5d94a1d6f91d9c9b819510b3e948
+plt.title("Experimental Duration")
+plt.show()
 
 ##print s/(len(k0)+len(k1)+len(k2))
 ##plt.plot(n0,k0,".")

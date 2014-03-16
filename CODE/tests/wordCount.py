0646ecc44a9c5d04e6d08ac6d9961105051bf2c3
Andrew Rosen <rosen@cs.gsu.edu>
2014-03-16 17:23:49 -0400

    bloop

--- CODE/tests/wordCount.py
+++ CODE/tests/wordCount.py
@@ -3,9 +3,7 @@ from map_reduce import Data_Atom
 
 
 def map_func(atom):
-    pass
     freqs = {}
-    
     text = atom.contents  #temporary for testing 
     # we'll have to pull the contents of the file for the given key
     
@@ -28,10 +26,14 @@ def reduce_func(atom1,atom2):
     else:
         raise Exception("unmatched jobs in reduce")
     "the form of this is probably wrong"
-    aFreqs = atom1.contents
-    bFreqs = atom2.contents
-    for word in bFreqs:
-        if 
+    a = atom1.contents
+    b = atom2.contents
+    for word in b:
+        if word in a:
+            a[word] = a[word] + b[word]
+        else:
+            a[word] = b[word]
+            
 
 def stage_func():
    pass

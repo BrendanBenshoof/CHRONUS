fbafaa8f5d9f4f18d9b04d72cacbdafca60959ee
Andrew Rosen <rosen@cs.gsu.edu>
2014-03-17 17:36:46 -0400

    Began iunstrumentation stuff

---
+++ CODE/instrumentation.py
@@ -0,0 +1,20 @@
+###
+## Experimental Values
+###
+
+# Sizes for the pi map reduce
+PI_JOB_LARGE = 100000000
+PI_JOB_MED   = 10000000
+PI_JOB_SMALL = 1000000
+
+
+# 
+
+
+
+###
+## Churn simulation
+###
+CHURN_RATE = 0.00025 # chance of churn per second
+ACTIVE_NODES = {}
+INACTIVE_NODES = {}

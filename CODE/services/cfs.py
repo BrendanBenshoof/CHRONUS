2586a68bb6478bc8fc5fd92548f8c05f767dadb8
BrendanBenshoof <brendan@glidr.net>
2014-03-17 09:57:51 -0400

    added cfs.py
    I think I am going to replace shelver and file service with what I am
    writing here.

---
+++ CODE/services/cfs.py
@@ -0,0 +1,19 @@
+#Chord File System
+"""
+TODO:
+
+write File
+    binary files
+    folders/archives?
+    manual or automatic chunking? both?
+
+read File
+    assemble recursively?
+    merge to binary file?
+    merge to tree of chunks?
+
+backup files
+    -backup scheme?
+
+maintain ownership of files????
+when do files expire? (never, but in general?)?
